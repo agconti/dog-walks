3a20f3c543e799f9743117117654d8ab14cc320c
agconti <agc11d@gmail.com>
2018-03-22 22:48:32 -0400

    feat(src/App): use dogGifFactory

--- walks/src/App.js
+++ walks/src/App.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { dogGifsFactory } from './gifs'
+import './App.css'
 
 const Detail = ({value}) => <span className='detail_item'>{value}</span>
 const Details = ({children}) => <div className='details'>{children}</div>
-const HeaderImage = () => {
-  const gif_ids = ['xULW8LkCv9QJMhyT7O', '3og0INJHs40dal3F0A', '26hlRAfYGFhM88uDm', 'sMaW02wUllmFi', '3oFzm6qEyneSv768Ny', 'WaDRFTy80J8u4', 'tdWsWGFHlW6as'
-  ]
-  const random_gif = gif_ids[~~(gif_ids.length * Math.random())]
 
-  return <img className='card__header__image'
-       src={`https://media1.giphy.com/media/${random_gif}/giphy.webp`} />
-}
 const Title = ({timeStamp, weekDay, address}) => {
   const date = new Date(timeStamp)
   const time  = date.getHours() > 12 ? 'afternoon': 'morning'
   return (
     <div className='card__title'>
       <header className='card__header'>
-          <HeaderImage />
+        <img className='card__header__image' src={dogGifsFactory()} alt='Dog Gif'/>
           <h2 className='time'>{date.toLocaleTimeString()}</h2>
           <h3 className='address'>{address}</h3>
       </header>
@@ -55,8 +49,8 @@ class App extends Component {
       <div className='walks'>
           {this.state.walks.map(walk => <WalkCard {...walk} key={walk.id}/>)}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
